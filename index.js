be0334963f00a55e58cae343c96153607d6e718d
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-05-13 13:05:03 +0600

    deploy on vercel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -23,16 +24,47 @@ const client = new MongoClient(uri, {
   },
 });
 
+//verify jwt
+const verifyJWT = (req, res, next) => {
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    // const database = client.db("carDoctorDB");
-    // const carCollection = database.collection("carDoctor");
     const servicesCollection = client.db("carDoctor").collection("services");
 
     const bookingCollection = client.db("carDoctor").collection("bookings");
+
+    //jwt JsonWebToken
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      });
+      console.log(token);
+
+      res.send({ token });
+    });
 
     //get data from collection or db (api)
     app.get("/services", async (req, res) => {
@@ -52,12 +84,17 @@ async function run() {
       };
 
       //booking collection get from db
-      app.get("/getbookings", async (req, res) => {
+      app.get("/getbookings", verifyJWT, async (req, res) => {
+        const decoded = req.decoded;
+        console.log('decoded inside', decoded);
+        if (decoded.email !== req.query.email) {
+          return res.status(403).send({error:1,message:'forbiden access'})
+        }
         let query = {};
         if (req.query?.email) {
           query = { email: req.query.email };
         }
-        const result = await bookingCollection.find().toArray();
+        const result = await bookingCollection.find(query).toArray();
         res.send(result);
       });
 
@@ -95,7 +132,6 @@ async function run() {
       });
 
       const result = await servicesCollection.findOne(query, options);
-      // const result=await servicesCollection.findOne({_id:new ObjectId(req.params.id)})
       res.send(result);
     });
 
